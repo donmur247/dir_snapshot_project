022b524e6d9b85e0a8eace8aa2fe05d2d42f6406
Donny Murdiyono <donnymurdiyono@gmail.com>
2025-01-19 00:28:30 -0800

    Simplify UI

--- dir_snapshot/app.py
+++ dir_snapshot/app.py
@@ -1,10 +1,56 @@
 """Application module for Directory Snapshot App."""
 
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import (
+    Footer,
+    Header,
+    Markdown,
+    OptionList,
+    SelectionList,
+    TabbedContent,
+    TabPane,
+)
 
 from dir_snapshot import APP_TITLE, APP_SUBTITLE, TCSS_DIR
-from dir_snapshot.ui import ContentWidget, DirListWidget, SnapshotListWidget
+
+
+# TODO: Temporary content. Remove them once we got dynamic content.
+COMPARISON_CONTENT = """
+# Comparison Result for Snapshot 1 and Snapshot 2
+
+## Added Directories
+- Directory 1
+- Directory 2
+
+## Added Files
+- File 1
+- File 2
+
+## Removed Directories
+- Directory 3
+- Directory 4
+
+## Removed Files
+- File 3
+- File 4
+"""
+
+HELP_CONTENT = """
+# Directory Snapshot Help
+
+## Getting Started
+The top area is for the list of directories.
+The bottom area is for the list of snapshots.
+
+## Adding a Directory
+Click the 'Add Directory' button to add a directory to the list.
+
+## Removing a Directory
+Click the 'Remove Directory' button to remove a directory from the list.
+
+## Comparing Snapshots
+Click the 'Compare Snapshots' button to compare the selected snapshots.
+"""
 
 
 class DirSnapshotApp(App):
@@ -17,7 +63,28 @@ class DirSnapshotApp(App):
 
     def compose(self) -> ComposeResult:
         yield Header()
-        yield DirListWidget(id="list-of-dirs")
-        yield SnapshotListWidget()
-        yield ContentWidget()
+        yield OptionList(
+            "C:/temp",
+            "C:/Program Files/Python310/Scripts",
+            "C:/Program Files (x86)/Common Files/Microsoft Shared/Office16",
+            "C:/Program Files (x86)/Common Files/Microsoft Shared/Office16/COM",
+        )
+        yield SelectionList(
+            ("Snapshot 1", 0, True),
+            ("Snapshot 2", 1),
+            ("Snapshot 3", 2),
+            ("Snapshot 4", 3),
+            ("Snapshot 5", 4),
+        )
+        with TabbedContent(initial="snapshot-result", id="content"):
+            with TabPane("Snapshot Results", id="snapshot-result"):
+                yield Markdown(COMPARISON_CONTENT, id="result-content")
+            with TabPane("Help", id="help"):
+                yield Markdown(HELP_CONTENT, id="help-content")
         yield Footer()
+
+    def on_mount(self) -> None:
+        self.query_one("#result-content").styles.height = "1fr"
+        self.query_one("#help-content").styles.height = "1fr"
+        self.query_one(OptionList).border_title = "Directories"
+        self.query_one(SelectionList).border_title = "Snapshots"
