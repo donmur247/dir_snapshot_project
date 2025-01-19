47dc64be13544d9d6750aef5934174c19210a94d
Donny Murdiyono <donnymurdiyono@gmail.com>
2025-01-18 17:29:27 -0800

    Add gutter and basic binding

--- dir_snapshot/app.py
+++ dir_snapshot/app.py
@@ -14,6 +14,7 @@ class DirSnapshotApp(App):
     CSS_PATH = (Path(__file__).parent / "styles" / "styles.tcss").as_posix()
     TITLE = APP_TITLE
     SUB_TITLE = APP_SUBTITLE
+    BINDINGS = [("q", "quit", "Quit")]
 
     def compose(self) -> ComposeResult:
         yield Header()
